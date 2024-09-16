392cd47032f83aca2d9de27a6a4deb0ef3b03d1e
PS <99ps.work@gmail.com>
2024-09-16 13:02:40 +0530

    Init

---
+++ hardhat.config.js
@@ -0,0 +1,81 @@
+require("@nomicfoundation/hardhat-toolbox");
+const CONFIG = require("./credentials.json");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        viaIR: true,
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200, // Adjust the number of runs as needed
+          },
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200, // Adjust the number of runs as needed
+          },
+        },
+      },
+      {
+        version: "0.7.5",
+        settings: {
+          optimizer: {
+            enabled: false,
+            // runs: 200, // Adjust the number of runs as needed
+          },
+        },
+      },
+    ],
+    viaIR: true,
+    // version: "0.8.19",
+    // settings: {
+    //   optimizer: {
+    //     enabled: true,
+    //     runs: 200, // Adjust the number of runs as needed
+    //   },
+    // },
+  },
+  networks: {
+    arbitrum: {
+			url: CONFIG["Mainnet_ARB"],
+			accounts: [CONFIG["PKEY"]],
+		},
+    goerli: {
+			url: CONFIG["GOERLI"]["URL"],
+			accounts: [CONFIG["GOERLI"]["PKEY"]],
+		},
+    hardhat: {
+      forking: {
+        url: CONFIG["Mainnet_ARB"], // Replace with your Alchemy API key
+      },
+    },
+  },
+  etherscan: {
+    apiKey: CONFIG["ARB_KEY"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // index of the account in the ethers.getSigners() array
+    },
+  },
+  sourcify: {
+    enabled: true
+  },
+  spdxLicenseIdentifier: {
+		overwrite: true,
+		runOnCompile: true,
+	},
+  contractSizer: {
+		alphaSort: false,
+		runOnCompile: true,
+		disambiguatePaths: false,
+	},
+};
